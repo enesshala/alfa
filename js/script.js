15d88a1112c669530e3b602ad1fb2f1fde4aed03
enesshala <51416158+enesshala@users.noreply.github.com>
2020-07-12 23:14:38 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-// Cursor
-let inner__cursor = document.querySelector(".inner__cursor");
-let outer__cursor = document.querySelector(".outer__cursor");
-let outer__cursor2 = document.querySelector(".outer__cursor2");
-
-document.onmousemove = function (e) {
-    inner__cursor.style.left = e.pageX - 5 + "px";
-    inner__cursor.style.top = e.pageY - 5 + "px";
-    inner__cursor.style.display = "block";
-
-    outer__cursor2.style.left = e.pageX - 12 + "px";
-    outer__cursor2.style.top = e.pageY - 12 + "px";
-    outer__cursor2.style.display = "block";
-
-    outer__cursor.style.left = e.pageX - 22 + "px";
-    outer__cursor.style.top = e.pageY - 22 + "px";
-    outer__cursor.style.display = "block";
-}
-
 // Preloader
 // function hidePreloader() {
 //     let preloader = document.getElementById('preloader');
@@ -36,3 +17,14 @@ function closeNav() {
     document.getElementById("myNav").style.height = "0%";
     document.getElementById("container").style.opacity = "1";
 }
+
+
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
